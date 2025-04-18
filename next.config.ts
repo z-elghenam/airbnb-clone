642624cd0716e1586a6d88941d2ce7c218215292
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-04-18 20:37:42 +0000

    feat(avatar): show avatar for logged-in users

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+  },
 };
 
 export default nextConfig;
