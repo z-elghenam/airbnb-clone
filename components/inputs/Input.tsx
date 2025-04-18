b517b7e47986a3c51660317ab9e98b9559d712da
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-04-18 20:38:17 +0000

    fix: fist ts error message

--- components/inputs/Input.tsx
+++ components/inputs/Input.tsx
@@ -1,29 +1,34 @@
-import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+import {
+  FieldErrors,
+  FieldValues,
+  UseFormRegister,
+  Path,
+} from "react-hook-form";
 import { BiDollar } from "react-icons/bi";
 
-interface InputProps {
-  id: string;
+interface InputProps<T extends FieldValues> {
+  id: Path<T>;
   label: string;
   type?: string;
   disabled?: boolean;
   formatPrice?: boolean;
   required?: boolean;
-  register: UseFormRegister<FieldValues>;
-  errors: FieldErrors;
+  register: UseFormRegister<T>;
+  errors: FieldErrors<T>;
 }
 
-const Input = ({
+const Input = <T extends FieldValues>({
   id,
   label,
-  type,
+  type = "text",
   disabled,
   formatPrice,
   required,
   register,
   errors,
-}: InputProps) => {
+}: InputProps<T>) => {
   return (
-    <div>
+    <div className="relative">
       {formatPrice && (
         <BiDollar
           size={24}
@@ -56,3 +61,62 @@ const Input = ({
 };
 
 export default Input;
+
+// import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+// import { BiDollar } from "react-icons/bi";
+
+// interface InputProps {
+//   id: string;
+//   label: string;
+//   type?: string;
+//   disabled?: boolean;
+//   formatPrice?: boolean;
+//   required?: boolean;
+//   register: UseFormRegister<FieldValues>;
+//   errors: FieldErrors;
+// }
+
+// const Input = ({
+//   id,
+//   label,
+//   type,
+//   disabled,
+//   formatPrice,
+//   required,
+//   register,
+//   errors,
+// }: InputProps) => {
+//   return (
+//     <div>
+//       {formatPrice && (
+//         <BiDollar
+//           size={24}
+//           className="text-neutral-700 absolute top-1/2 left-2"
+//         />
+//       )}
+//       <label
+//         className={`text-lg font-semibold ${
+//           errors[id] ? "text-rose-500" : "text-zinc-500"
+//         }`}
+//       >
+//         {label}
+//       </label>
+//       <input
+//         type={type}
+//         id={id}
+//         disabled={disabled}
+//         {...register(id, { required })}
+//         placeholder=""
+//         className={`peer w-full p-4 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed
+//           ${formatPrice ? "pl-9" : "pl-4"}
+//           ${
+//             errors[id]
+//               ? "border-rose-500 focus:border-rose-500"
+//               : "border-neutral-300 focus:border-neutral-700"
+//           }`}
+//       />
+//     </div>
+//   );
+// };
+
+// export default Input;
