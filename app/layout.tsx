817e6daa0d2051917a1bed9c561a87ec0bc8ae5b
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-04-18 20:30:04 +0000

    feat: fetch currentuser in layout and then  pass currentUser to Navbar from Navbar to UserMenu

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/navbar/Navbar";
 import { Toaster } from "react-hot-toast";
 import RegisterModal from "@/components/modals/RegisterModal";
 import LoginModal from "@/components/modals/LoginModal";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 
 export const metadata: Metadata = {
   title: "Airbnb | Home",
@@ -13,18 +14,20 @@ export const metadata: Metadata = {
 
 const font = Nunito({ subsets: ["latin"] });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
         <Toaster />
         <LoginModal />
         <RegisterModal />
-        <Navbar />
+        <Navbar currentUser={currentUser} />
         <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,8 +2,14 @@ import Container from "@/components/Container";
 import Logo from "@/components/navbar/Logo";
 import Search from "@/components/navbar/Search";
 import UserMenu from "@/components/navbar/UserMenu";
+import { User } from "@prisma/client";
+import Categories from "./Categories";
 
-const Navbar = () => {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar = ({ currentUser }: NavbarProps) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
@@ -11,10 +17,11 @@ const Navbar = () => {
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
             <Search />
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 };
