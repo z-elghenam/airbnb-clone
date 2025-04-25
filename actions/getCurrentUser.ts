4d9446b846c06a9e5103f6dc713d6972e9f5a4d2
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-04-25 00:57:05 +0000

    rename the file

--- actions/getCurrentUser.ts
+++ actions/getCurrentUser.ts
@@ -19,7 +19,12 @@ export const getCurrentUser = async () => {
       return null;
     }
 
-    return currentUser;
+    return {
+      ...currentUser,
+      createdAt: currentUser.createdAt.toISOString(),
+      updatedAt: currentUser.updatedAt.toISOString(),
+      emailVerified: currentUser.emailVerified?.toISOString() || null,
+    };
   } catch {
     return null;
   }
